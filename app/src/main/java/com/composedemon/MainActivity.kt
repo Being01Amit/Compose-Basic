df9152d8e27bcefc82bc9576a1f9fcca16d42745
Being01Amit <amitsunar54321@gmail.com>
2023-12-17 22:21:48 +0530

    "Row and Column Done"

--- app/src/main/java/com/composedemon/MainActivity.kt
+++ app/src/main/java/com/composedemon/MainActivity.kt
@@ -4,26 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
-import com.composedemon.ui.theme.ComposeDemonTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -77,19 +77,42 @@ fun mainScreen() {
         color = Color.DarkGray,
         modifier = Modifier.fillMaxSize()
     ) {
-
-        /*This surface contain the Text view inside it*/
-        Surface(
-            color = Color.Cyan,
-            modifier = Modifier.wrapContentSize()
+        /*In Column and Row we can use the multiple surfaces or views*/
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.SpaceEvenly,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(
-                text = " Hello Main Screen",
-                modifier = Modifier
-                    .padding(20.dp),
-                style = MaterialTheme.typography.headlineMedium
-            )
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                /*Here we are using the same composable Surface function again for code re-usability*/
+                colorBox(Color.Yellow)
+                colorBox(Color.Magenta)
+            }
+//          Here we are using the same composable Surface function again for code re-usability
+            colorBox(Color.Cyan)
+            colorBox(Color.Blue)
+            colorBox(Color.Red)
         }
+    }
+}
+
+@Composable
+fun colorBox(color: Color) {
+    /*This surface contain the Text view inside it*/
+    Surface(
+        color = color,
+        /*Here we can align the Surface inside  wrapContentSize */
+        modifier = Modifier
+            .height(80.dp)
+            .width(80.dp)
+    ) {
+        /*Text(
+            text = " Hello Main Screen",
+            style = MaterialTheme.typography.headlineMedium
+        )*/
     }
 }
 
